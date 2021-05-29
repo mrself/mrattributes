db7bbe4a884c871be7651f434f58424648251e6f
Yaroslav Vavrysh <yaruk.my@gmail.com>
2021-05-29 21:47:31 +0300

    create testItReturnsMultipleEntitiesOnAllLevels

--- tests/Collection/ParentChildTest/MultipleOneLevelTest.php
+++ tests/Collection/ParentChildTest/MultipleOneLevelTest.php
@@ -128,6 +128,129 @@ class MultipleOneLevelTest extends Test
         $this->assertCount(1, $result);
         $this->assertEquals(2, $result[0]->count());
     }
+
+    public function testItReturnsMultipleEntitiesOnAllLevels()
+    {
+        $parent1 = new class extends HasParentEntity {
+            public DoctrineCollection $children;
+
+            public function getId(): int
+            {
+                return 1;
+            }
+
+            public function getChildren(): DoctrineCollection
+            {
+                return $this->children;
+            }
+        };
+
+        $parent2 = new class extends HasParentEntity {
+            public function getId(): int
+            {
+                return 2;
+            }
+        };
+
+        $child1 = new class extends HasParentEntity {
+            public $parent;
+
+            public function getParent()
+            {
+                return $this->parent;
+            }
+
+            public function getId(): int
+            {
+                return 3;
+            }
+
+        };
+
+        $childrenParent = new class extends HasParentEntity {
+            public $parent;
+
+            public DoctrineCollection $children;
+
+            public function getParent()
+            {
+                return $this->parent;
+            }
+
+            public function getId(): int
+            {
+                return 4;
+            }
+
+            public function getChildren(): DoctrineCollection
+            {
+                return $this->children;
+            }
+        };
+
+        $parent1->children = new ArrayCollection([$child1, $childrenParent]);
+        $child1->parent = $parent1;
+        $childrenParent->parent = $parent1;
+
+        $child2 = new class extends HasParentEntity {
+            public $parent;
+
+            public function getParent()
+            {
+                return $this->parent;
+            }
+
+            public function getId(): int
+            {
+                return 5;
+            }
+
+        };
+
+        $child3 = new class extends HasParentEntity {
+            public $parent;
+
+            public function getParent()
+            {
+                return $this->parent;
+            }
+
+            public function getId(): int
+            {
+                return 6;
+            }
+
+        };
+
+        $childrenParent->children = new ArrayCollection([$child2, $child3]);
+        $child2->parent = $childrenParent;
+        $child3->parent = $childrenParent;
+
+
+        $entities = [
+            $parent1,
+            $parent2,
+            $child1,
+            $childrenParent,
+            $child2,
+            $child3,
+        ];
+        $result = Collection::from($entities)->findMultipleOneLevelAttributes();
+
+        $this->assertCount(3, $result);
+
+        $roots = $result[0];
+        $this->assertEquals(1, $roots[0]->getId());
+        $this->assertEquals(2, $roots[1]->getId());
+
+        $secondLevel = $result[1];
+        $this->assertEquals(3, $secondLevel[0]->getId());
+        $this->assertEquals(4, $secondLevel[1]->getId());
+
+        $thirdLevel = $result[2];
+        $this->assertEquals(5, $thirdLevel[0]->getId());
+        $this->assertEquals(6, $thirdLevel[1]->getId());
+    }
 }
 
 class Collection extends AppCollection
